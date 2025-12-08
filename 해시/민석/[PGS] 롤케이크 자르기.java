ce60bffdb29a702d60a99c6f83b3d48f8e222807
whkalstjr <darth0324@naver.com>
2025-12-08 16:57:35 +0900

    [PGS] 롤케이크 자르기 수정

--- 해시/민석/[PGS] 롤케이크 자르기.java
+++ 해시/민석/[PGS] 롤케이크 자르기.java
@@ -16,7 +16,7 @@ class Solution {
         for(int i = 0 ;i<topping.length  ;i++){  //1~4
             //자르려 하면 안되나 ? 자르는 순간부터 n의 2승임 -=> 1초넘어감 
              
-             map1.put(topping[i],map2.getOrDefault(topping[i],0)+1);
+             map1.put(topping[i],map1.getOrDefault(topping[i],0)+1);
               map2.put(topping[i],map2.getOrDefault(topping[i],0)-1);
             if(map2.get(topping[i]) == 0)map2.remove(topping[i]);
             
